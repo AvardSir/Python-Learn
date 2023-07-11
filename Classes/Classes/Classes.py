256c3d88ecbaebe344777afa6549c795ff3e0669
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-07-11 23:14:59 +0300

    super() in Class

--- Classes/Classes/Classes.py
+++ Classes/Classes/Classes.py
@@ -1,11 +1,40 @@
 
-class h_class(object):
-    def __init__(self,num_of_h=3) -> None:
-        print("h")
-        self.num_of_h=num_of_h
-        print(num_of_h)
+#from collections import _OrderedDictValuesView
 
 
-h=h_class(2)
+#class h_class(object):
+#    def __init__(self,num_of_h=3) -> None:
+#        print("h")
+#        self.num_of_h=num_of_h
+#        print(num_of_h)
 
-h2=h_class()
+
+#h=h_class(2)
+
+#h2=h_class()
+
+class animal:
+    def __init__(self,color):
+        self.color=color
+    def sound(self):
+        print('kz kz kz')
+
+class dog(animal):
+    def sound(self):
+        
+        print('BARK BARK')
+        super().sound()
+
+Ovcharka=dog("red")
+Ovcharka.sound()
+
+class A:
+    def spam(self):
+        print(1)
+
+class B(A):
+    def spam(self):
+        print(2)
+        super().spam()
+
+#B().spam()
