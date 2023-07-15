80695f80a24d81ec39c8cb8112b985460fd6d219
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-07-15 14:21:23 +0300

    Property

---
+++ Properties/Properties/Properties.py
@@ -0,0 +1,19 @@
+class rabit():
+    def __init__(self,color,mass):
+        self.color=color
+        self.mass=mass
+    @property#пропертти, но плохо ясно зачем
+    def change_mass(self):
+        self.mass+=10
+
+    @property
+    def mass_allowd(self):
+        return self.mass
+    @mass_allowd.setter
+    def mass_allowd(self,new_mass):
+        self.mass+=new_mass
+
+rab1=rabit('white',20)
+print(rab1.mass_allowd)
+rab1.mass_allowd(10)
+print(rab1.mass_allowd)
