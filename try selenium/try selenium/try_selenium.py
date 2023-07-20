3f6472457867cca9631adc5097c9018d174d08ab
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-07-20 20:43:33 +0300

    Try salenium

---
+++ try selenium/try selenium/try_selenium.py
@@ -0,0 +1,20 @@
+
+#import time
+#import os
+#from selenium import webdriver
+#from selenium.webdriver.chrome.service import Service
+#from selenium.webdriver.common.by import By
+#from selenium.webdriver.common.keys import Keys
+#s = Service (executable_path='C:\Users\user\Downloads\chromedriver-win64') driver = webdriver. Chrome (service=s)
+#try:
+#    driver.maximize_window()
+#    driver.get('https://vk.com')
+#    time.sleep(5)
+#except Exception as ex:
+#    print(ex)   
+#finally:
+#    driver.close()
+#    driver.quit()
+
+#print('привет')
+print(u'\u0439')
