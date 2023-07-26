1689fef455415162357e32984e8eaa75973c4653
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-07-26 11:48:02 +0300

    repetition dict

---
+++ intermidiant/intermidiant/intermidiant.py
@@ -0,0 +1,21 @@
+def check1():
+    n = [2, 4, 6, 8]
+    res = 1
+    for x in n[1:3]:
+      res *= x
+
+    print(res)
+
+def dict_():
+
+    x = ['hi', 'hello', 'welcome']
+    print(x[2])
+
+def dict_2():
+    ages = {
+       "Dave": 24,
+       "Mary": 42,
+       "John": 58
+    }
+    print(ages["Dave"])
+    print(ages["Mary"])
