735c8ceaba411ad5e30be8d8f7a37ffdaeb3e2fd
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-07-27 00:45:29 +0300

    get and in with    dict

---
+++ dict func/dict func/dict_func.py
@@ -0,0 +1,24 @@
+def dict_specific():
+    nums = {
+      1: "one",
+      2: "two",
+      3: "three",
+    }
+    print(1 in nums)
+    print("three" in nums)
+    print(4 not in nums)
+def get_method():
+
+    pairs = {
+       1: "apple",
+       "orange": [2, 3, 4], 
+       True: False, 
+       12: "True",
+    }
+
+    print(pairs.get("orange"))
+    print(pairs['orange'])
+    print(pairs.get(7, 42))
+    print(pairs.get(12345, "not found"))
+
+    print(pairs)
