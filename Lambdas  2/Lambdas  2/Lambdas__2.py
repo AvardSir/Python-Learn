732f906791da34fd4f8cfd7ec11bca6b97de5692
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-08-02 11:42:16 +0300

    lamda retry

---
+++ Lambdas  2/Lambdas  2/Lambdas__2.py
@@ -0,0 +1,12 @@
+def lam1():
+    def my_func(f, arg):
+      return f(arg)
+
+    print(my_func(lambda x: 2*x*x, 5))
+
+def polynomial(x):
+    return x**2 + 5*x + 4
+print(polynomial(-4))
+
+#lambda
+print((lambda x: x**2 + 5*x + 4) (-4))
