e723ecdfb643100bbc9436e3d70b2b271f564c64
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-08-05 12:41:13 +0300

    done decor task

--- Decorators  2/Decorators  2/Decorators__2.py
+++ Decorators  2/Decorators  2/Decorators__2.py
@@ -17,9 +17,9 @@ def decor1():
 
 
 def decor(func):
-    def wrap():
+    def wrap(*args, **kwargs):
         print("***")
-        func()
+        func(*args, **kwargs)
         print("***")
     return wrap
 #your code goes here
