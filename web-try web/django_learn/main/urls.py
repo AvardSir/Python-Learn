016e46ea5ccc571979678a8dd09acf0a85de9e14
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-08-13 10:22:56 +0300

    Вывод текста, urls= простейшее создание связей url

--- web-try web/django_learn/main/urls.py
+++ web-try web/django_learn/main/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+#from django.contrib import admin
+#path('admin/', admin.site.urls),
+#    path('', include('main.urls'))
 from django.urls import path,include
-
+from. import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', views.hmm),
+    path('about', views.about),
+    path('lol',views.lol)
 ]
--- web-try web/django_learn/main/views.py
+++ web-try web/django_learn/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+#def index():
 
 # Create your views here.
+def hmm(request):
+    return HttpResponse("<h4>Пpoверка работы</h4>")
+    #print('hi!') # нету ттут принта....
+
+def about(request):
+    return HttpResponse("<h4>ЭБАУНТ РАБОТАЕТ</h4>")
+
+def lol(request):
+    return HttpResponse("<h4>lol</h4>")
