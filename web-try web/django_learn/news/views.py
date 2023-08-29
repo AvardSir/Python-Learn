6314978d9f912843568a272d0b49ec65e2ba95df
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-08-29 11:06:13 +0300

    for i in range  NOT WORK

--- web-try web/django_learn/news/views.py
+++ web-try web/django_learn/news/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def news_hone(request):
-    return render(request, 'main/about.html')#KAT
+    return render(request, 'main/contact.html')
