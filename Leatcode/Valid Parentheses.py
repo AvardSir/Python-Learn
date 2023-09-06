4880b602a665fc75428662ed7bf2ba33585e5d3d
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-09-06 15:57:11 +0300

    ug

--- Leatcode/Valid Parentheses.py
+++ Leatcode/Valid Parentheses.py
@@ -1,29 +1,31 @@
 def opredelim_obr(symbol):
-    diction={'(':')','[':']','{':'}'}
+    diction = {'(': ')', '[': ']', '{': '}'}
     return diction[symbol]
 
-def brecket(word,symbol):
+
+def brecket(word, symbol):
     print(len(word))
-    left_sybmols=['(','{','[']
-    right_sumbols=[opredelim_obr(i) for i in left_sybmols]
+    left_sybmols = ['(', '{', '[']
+    right_sumbols = [opredelim_obr(i) for i in left_sybmols]
     if symbol in right_sumbols:
         return False
 
-    revers_symbol=opredelim_obr(symbol)
+    revers_symbol = opredelim_obr(symbol)
     for i in range(len(word)):
-        if word[i] in left_sybmols and word[i]!=revers_symbol or word[i]==revers_symbol:
+        if word[i] in left_sybmols and word[i] != revers_symbol or word[i] == revers_symbol:
             return brecket(word[1:len(word)], word[i])
         else:
             return False
     return True
+
+
 def first_in_brecket(word):
-    if len(word)<2:
+    if len(word) < 2:
         return False
     else:
 
-        return brecket(word[1:len(word)],word[0])
+        return brecket(word[1:len(word)], word[0])
 
 
-
-word='(){]'
-print(first_in_brecket(word))#AAAA
+word = '(){]'
+print(first_in_brecket(word))
---
+++ Leatcode/graph.py
@@ -0,0 +1,18 @@
+class graph():
+    def __init__(self,graphs=[]):
+        self.graphs = graphs
+        self.graphs = [[1, 2], [2, 3], [3, 1], [0, 0]]
+        print('we creat')
+    def print_graph(self):
+        for i in self.graphs:
+            print(i)
+def graph():
+    pass
+
+
+def print_graph(graphs):
+    pass
+
+first_graph=graph()
+first_graph.print_graph()
+
