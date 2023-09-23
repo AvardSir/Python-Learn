9d96329c3184c4742a45881db42b88399b267f51
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-09-23 10:00:57 +0300

    get len for graph

--- Leatcode/graph.py
+++ Leatcode/graph.py
@@ -8,11 +8,12 @@ class graph():
             print(i)
     def add_path(self,new_path):
         self.graphs.append(new_path)
-
+    def get_len(self):
+        return len(self.graphs)
 first_graph=graph([1,23])
 first_graph.add_path([0,7])
 first_graph.print_graph()#tak tak#####YEK voyna
-
+print(first_graph.get_len())
 #voyna is bad#so bad so b#####a##########Im sick#alsi sick!also sick#SSS#LABAROTIRY#ffsffssf#im in so bad moon#
 
 #fuck it so bad lost 70 day steak..###So bad its in divorce###AAAA
