7a3f2c7c82c8638ec0cdd59d4e9943d378eeeb52
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-09-24 10:51:02 +0300

    add base graph

--- Leatcode/graph.py
+++ Leatcode/graph.py
@@ -1,7 +1,9 @@
 class graph():
-    def __init__(self,graphs=[]):
-        self.graphs = graphs
-        self.graphs = [[1, 2], [2, 3], [3, 1], [0, 0]]
+    def __init__(self,graphs=[[1, 2], [2, 3], [3, 1], [0, 0]]):
+
+        self.graphs = [graphs]
+
+        #self.graphs = [[1, 2], [2, 3], [3, 1], [0, 0]]
         print('we creat')
     def print_graph(self):
         for i in self.graphs:
@@ -12,8 +14,9 @@ class graph():
         return len(self.graphs)
 first_graph=graph([1,23])
 first_graph.add_path([0,7])
-first_graph.print_graph()#tak tak#####YEK voyna
+#tak tak#####YEK voyna
 print(first_graph.get_len())
+first_graph.print_graph()
 #voyna is bad#so bad so b#####a##########Im sick#alsi sick!also sick#SSS#LABAROTIRY#ffsffssf#im in so bad moon#
 
 #fuck it so bad lost 70 day steak..###So bad its in divorce###AAAA
