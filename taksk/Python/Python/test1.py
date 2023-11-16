44a001ec51da3383c508d188ee26928c3e6dead2
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-11-16 22:22:29 +0300

    >>> import turtle DRAW a circle

--- taksk/Python/Python/test1.py
+++ taksk/Python/Python/test1.py
@@ -8,4 +8,20 @@ class Items(object):
     def add(self):
         return "Braking"
 print('go')
+####
 ##
+import turtle
+
+def draw_circle():
+    window = turtle.Screen()
+    window.bgcolor("white")
+
+    circle = turtle.Turtle()
+    #circle.shape("turtle circle.color")
+    circle.speed(2)
+
+    circle.circle(100)
+
+    turtle.done()
+
+draw_circle()
