11d526c357831881609b077f00d40a09690e51ad
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-12-16 18:52:12 +0300

    разбивает строку убирает пробелы

--- Django_Try/456.py
+++ Django_Try/456.py
@@ -1,34 +1,8 @@
-def lol1():
-
-    import math
-    import random
-    import numpy
-
-    import matplotlib.pyplot as plt
-    def telecom1():
-
-        R=10
-        print(random.random() * R * R)
-
-    R=10
-    print(R**2)
-
-#python#
-import tkinter as tk
-from tkinter import messagebox
-
-# Функция, которая будет вызываться при нажатии на кнопку
-def show_message():
-    messagebox.showinfo("Сообщение", "Привет, тебе!")
-
-# Создание основного окна
-window = tk.Tk()
-
-# Создание кнопки
-button = tk.Button(window, text="Нажми меня", command=show_message)
-
-# Размещение кнопки на окне
-button.pack()
-
-# Запуск основного цикла обработки событий
-window.mainloop()
+a=3
+b=2
+c=1
+print(a+b+c)
+words='asfk asfasf'
+words = words.split()
+joined_string = "".join(words)# разбивает строку убирает пробелы
+print(joined_string)
