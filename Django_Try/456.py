43bef34d2e43561030543da17fc75f3748f0a7dd
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-12-17 23:42:05 +0300

    создает окно с двумя кнопками которые меняют цвет при их нажатии

--- Django_Try/456.py
+++ Django_Try/456.py
@@ -1,8 +1,36 @@
-a=3
-b=2
-c=1
-print(a+b+c)
-words='asfk asfasf'
-words = words.split()
-joined_string = "".join(words)# разбивает строку убирает пробелы
-print(joined_string)
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt5.QtGui import QColor, QPalette
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Change Color")
+        self.setGeometry(100, 100, 300, 200)
+
+        self.button1 = QPushButton("Button 1", self)
+        self.button1.setGeometry(50, 50, 100, 50)
+        self.button1.clicked.connect(self.change_color1)
+
+        self.button2 = QPushButton("Button 2", self)
+        self.button2.setGeometry(150, 50, 100, 50)
+        self.button2.clicked.connect(self.change_color2)
+
+        self.default_palette = self.palette()
+
+    def change_color1(self):
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor("red"))
+        self.setPalette(palette)
+
+    def change_color2(self):
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor("blue"))
+        self.setPalette(palette)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
