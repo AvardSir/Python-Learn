78dad3966825fc20b07e8b136d7ea8b3d15e6d88
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-12-28 13:25:42 +0300

    round planet

--- Django_Try/planet.py
+++ Django_Try/planet.py
@@ -17,8 +17,8 @@ FONT = pygame.font.SysFont("comicsans", 16)
 #print('yo')
 
 class Planet:
-    AU = 149.6e6 * 1000
-    G = 3.67428e-11#6.67428e-11
+    AU = 100.6e6 * 1000#149.6e6 * 1000
+    G = 3.67428e-11#6.67428e-11 
     SCALE = 250 / AU  # 1AU = 100 pixels
     TIMESTEP = 3600 * 24  # 1 day
 
