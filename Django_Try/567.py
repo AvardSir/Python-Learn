82bb6f1306a09deb9dc7c03a9a226fa86ef491f4
AvardSir <114823780+AvardSir@users.noreply.github.com>
2023-12-31 12:03:23 +0300

    screen_width  screen_height

--- Django_Try/567.py
+++ Django_Try/567.py
@@ -4,8 +4,8 @@ import pygame
 pygame.init()
 
 # Размеры окна
-screen_width = 800
-screen_height = 600
+screen_width = 1200
+screen_height = 1000
 
 # Создание окна
 screen = pygame.display.set_mode((screen_width, screen_height))
