a7b8656a1421c6b0baa67a1d6be0d2a809d9dfe0
AvardSir <114823780+AvardSir@users.noreply.github.com>
2024-01-25 08:51:16 +0300

    class

--- Django_Try/567.py
+++ Django_Try/567.py
@@ -1,1 +1,13 @@
-class class class 
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Привет, меня зовут {self.name} и мне {self.age} лет.")
+
+# Создаем экземпляр класса Person
+person1 = Person("Иван Лашин", 21)
+
+# Вызываем метод greet для экземпляра person1
+person1.greet()
