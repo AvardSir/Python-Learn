6ebf5a7a948af83c3ae40ce86ce86abe11e0b0e3
AvardSir <114823780+AvardSir@users.noreply.github.com>
2024-03-07 11:15:31 +0300

    bad drop first colum

--- Django_Try/123.py
+++ Django_Try/123.py
@@ -1,3 +1,4 @@
+
 def groupa():
 
     # N = (номер группы + номер студента в журнале) mod количество_вариантов + 1.
@@ -10,7 +11,21 @@ def groupa():
 
 groupa()
 
-class Person:#qjjj
+import pandas as pd
+def pandas_kill_first_colom():
+
+
+
+    # Создание DataFrame
+    data = {'A': [1, 2, 3], 'B': [4, 5, 6], 'C': [7, 8, 9]}
+    df = pd.DataFrame(data)
+
+    # Удаление первого столбца
+    df = df.drop(df.columns[0], axis=1)
+    print(df)
+pandas_kill_first_colom()
+
+class Person:#qjjj#
 
     def __init__(self, listt):
         self.listt=listt
@@ -27,8 +42,3 @@ class Person:#qjjj
 # Создание объекта (экземпляра) класса Person
 person1 = Person([1,23,4])
 
-# Вызов метода для отображения информации о персоне
-person1.out_list()
-
-l=[1,2,3]
-person1.sum_list(l)
