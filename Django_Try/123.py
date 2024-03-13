a968297cb5b0c988e44d5b2b0611f8924ac649a3
AvardSir <114823780+AvardSir@users.noreply.github.com>
2024-03-13 17:49:59 +0300

    424

--- Django_Try/123.py
+++ Django_Try/123.py
@@ -29,7 +29,7 @@ def pandas_kill_first_colom():
     df = df.drop(df.columns[0], axis=1)
     print(df)#dsls;l
 pandas_kill_first_colom()
-def kilas():
+def kilas():#aslsl
     import pandas as pd
 
     # Создание примера DataFrame
