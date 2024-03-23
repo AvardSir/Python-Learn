4d954781355dcb5e76b58309dc9a2cb089a6e099
AvardSir <114823780+AvardSir@users.noreply.github.com>
2024-03-23 19:13:31 +0300

    class work start

--- Django_Try/123.py
+++ Django_Try/123.py
@@ -1,9 +1,15 @@
-import pandas as pd
 
-#create DataFrame
-df = pd.DataFrame({'A': [25, 12, 15, 14, 19, 23, 25, 29],
- 'B': [5, 7, 7, 9, 12, 9, 9, 4],
- 'C': [11, 8, 10, 6, 6, 5, 9, 12]})
-df.drop('B', axis= 1 , inplace= True )
-print(df)
-print(df)#daslsl
+
+
+class body:
+ hp = 30
+ def __init__(self):
+  pass
+  #self.hp = 30
+
+ def hand_up(self):
+  print('hend up!')
+  print(self.hp)
+
+a=body()
+a.hand_up()
